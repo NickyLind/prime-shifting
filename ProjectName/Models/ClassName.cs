4645910076825cafdf7e4a7b29bbfaf3aea392f6
NickyLind <76918982+NickyLind@users.noreply.github.com>
2021-02-24 14:36:00 -0800

    Initial commit

---
+++ ProjectName/Models/ClassName.cs
@@ -0,0 +1,9 @@
+// In this class file, make sure the namespace matches the name of your project (the equivalent of ProjectName). For instance:
+
+namespace ProjectName
+{
+  public class ClassName
+  {
+//     properties, constructors, methods, etc. go here
+  }
+}
